fa7df0168db13c3e5c2a8b7318b628c9386f1cc5
Jérôme Quelin <jquelin@gmail.com>
2010-01-06 13:04:12 +0100

    applying plugins that i want in the bundle

--- lib/Dist/Zilla/PluginBundle/JQUELIN.pm
+++ lib/Dist/Zilla/PluginBundle/JQUELIN.pm
@@ -5,6 +5,7 @@ use warnings;
 package Dist::Zilla::PluginBundle::JQUELIN;
 # ABSTRACT: build & release a distribution like jquelin
 
+use Dist::Zilla::PluginBundle::Git;
 use Moose;
 use Moose::Autobox;
 
@@ -12,18 +13,81 @@ with 'Dist::Zilla::Role::PluginBundle';
 
 sub bundle_config {
     my ($self, $section) = @_;
-    my $class = ( ref $self ) || $self;
+    my $class = ref($self) || $self;
     my $arg   = $section->{payload};
 
-    # bundle all git plugins
-    my @classes =
-        map { "Dist::Zilla::Plugin::Git::$_" }
-        qw{ Check Commit Tag Push };
+    # params for AutoVersion
+    my $major_version  = defined $arg->{major_version} ? $arg->{major_version} : 1;
+    my $version_format =
+          q<{{ $major }}.{{ cldr('yyDDD') }}>
+        . sprintf('%01u', ($ENV{N} || 0))
+        . ($ENV{DEV} ? (sprintf '_%03u', $ENV{DEV}) : '');
+
+    # long list of plugins
+    my @wanted = (
+        # -- static meta-information
+        [   AutoVersion => {
+                major     => $major_version,
+                format    => $version_format,
+                time_zone => 'Europe/Paris',
+            }
+        ],
+
+        # -- fetch & generate files
+        [ AllFiles     => {} ],
+        [ CompileTests => {} ],
+        [ CriticTests  => {} ],
+        [ MetaTests    => {} ],
+        [ PodTests     => {} ],
+
+        # -- remove some files
+        [ PruneCruft   => {} ],
+        [ ManifestSkip => {} ],
+
+        # -- get prereqs
+        [ AutoPrereq => {} ],
+
+        # -- munge files
+        [ ExtraTests  => {} ],
+        [ NextRelease => {} ],
+        [ PkgVersion  => {} ],
+        [ PodWeaver   => {} ],
+        [ Prepender   => { copyright => 1 } ],
+
+        # -- dynamic meta-information
+        [ InstallDirs             => {} ],
+        [ 'MetaProvides::Package' => {} ],
+
+        # -- generate meta files
+        [ License     => {} ],
+        [ ModuleBuild => {} ],
+        [ MetaYAML    => {} ],
+        [ Readme      => {} ],
+        [ Manifest    => {} ], # should come last
+
+        # -- release
+        [ CheckChangeLog => {} ],
+        #[ @Git],
+        [ UploadToCPAN   => {} ],
+    );
+
+    # create list of plugins
+    my $prefix = 'Dist::Zilla::Plugin::';
+    my @plugins =
+        map { [ "$class/$prefix$_->[0]" => "$prefix$_->[0]" => $_->[1] ] }
+        @wanted;
+
+    # add git plugins
+    my @gitplugins = Dist::Zilla::PluginBundle::Git->bundle_config( {
+        name    => "$class/Git",
+        payload => { },
+    } );
+    push @plugins, @gitplugins;
 
     # make sure all plugins exist
-    eval "require $_; 1" or die for @classes; ## no critic ProhibitStringyEval
+    eval "require $_->[1]" or die for @plugins; ## no critic ProhibitStringyEval
 
-    return @classes->map(sub { [ "$class/$_" => $_ => $arg ] })->flatten;
+    return @plugins;
 }
 
 
@@ -40,7 +104,7 @@ __END__
 In your F<dist.ini>:
 
     [@JQUELIN]
-    major = 1        ; this is the default
+    major_version = 1        ; this is the default
 
 =head1 DESCRIPTION
 
@@ -87,6 +151,6 @@ equivalent to:
     [@Git]
     [UploadToCPAN]
 
-The C<major> option is passed to the
+The C<major_version> option is passed as C<major> option to the
 L<AutoVersion|Dist::Zilla::Plugin::AutoVersion> plugin.
 
